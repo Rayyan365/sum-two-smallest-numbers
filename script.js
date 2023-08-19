d5aaa42b15ade034ba11af2f6a2ddd92564b8ba4
Rayyan365 <rayanabdallah09@gamil.com>
2023-08-19 07:20:57 -0700

    sum-two-smallest-numbers assignment done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Good Luck 💪🏾
+numbers[10,66.765,345,44,24,245,63,23,54]
+
+function sumTwoSmallestNumbers(numbers) 
+
+{  
+ a = Math.min.apply(Math, numbers); 
+ numbers.splice(a);
+ b = Math.min.apply(Math, numbers);
+ return (a + b) 
+}
